1812131a6a5b326f1753c2880516e2440ddb841b
gromph <bensnajdar@posteo.net>
2025-11-12 13:03:51 +0100

    05 mem alloc

--- 02_catch_up/05_mem_alloc.c
+++ 02_catch_up/05_mem_alloc.c
@@ -8,5 +8,41 @@ int main() {
 
     printf("Enter a integer sequence (stop with -1): ");
     
+    int *seq = malloc(capacity * sizeof(int));
+    if (!seq) {
+        perror("Couldn't allocate memory!");
+        exit(EXIT_FAILURE);
+    }
     
+    int buffer = 0;
+
+    while (buffer != -1) {
+        scanf("%i", &buffer);
+        seq[position] = buffer;
+        position++;
+
+        if (position >= capacity) {
+            size_t new_cap = capacity + 4;
+            int *tmp = realloc(seq, new_cap *sizeof(int));
+            if (!tmp) {
+                perror("Couldn't allocate memory!");
+                free(seq);
+                exit(EXIT_FAILURE);
+            }
+            seq = tmp;
+            free(tmp);
+            capacity = new_cap;
+        }
+    }
+
+    printf("Seq: ");
+    for(int i=0; i < position; i++) {
+        printf("%i ", seq[i]);
+    }
+    printf("\n");
+
+
+    free(seq);
+
+    return 0;
 }
